ef9015ccd73a11779c91a258e10d0a70d7731765
Steven Landow <landow@google.com>
2020-07-14 11:16:42 -0500

    break test by deleting service

--- tests/integration/pilot/serviceentry_selector_test.go
+++ tests/integration/pilot/serviceentry_selector_test.go
@@ -1,6 +1,7 @@
 package pilot
 
 import (
+	"context"
 	"fmt"
 	"istio.io/istio/pkg/config/protocol"
 	"istio.io/istio/pkg/test/echo/common/scheme"
@@ -8,6 +9,7 @@ import (
 	"istio.io/istio/pkg/test/framework/components/echo"
 	"istio.io/istio/pkg/test/framework/components/echo/echoboot"
 	"istio.io/istio/pkg/test/framework/components/namespace"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 )
 
@@ -68,6 +70,11 @@ func TestServiceEntryWorkloadSelectors(t *testing.T) {
 					}},
 				}).
 				BuildOrFail(ctx)
+
+			// we're testing that workloadSelector works without a service
+			if err := ctx.Clusters()[0].CoreV1().Services(ns.Name()).Delete(context.TODO(), serviceName, v1.DeleteOptions{}); err != nil {
+				ctx.Fatalf("failed to delete service %s/%s: %v", ns.Name(), serviceName, err)
+			}
 
 			ctx.Config().ApplyYAMLOrFail(ctx, ns.Name(), fmt.Sprintf(SidecarConfig, ns.Name(), serviceName))
 
